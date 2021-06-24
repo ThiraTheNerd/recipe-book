c5ae8454cddac87f2a9ba2d76fc7b3fb4b9df13e
Naomi Chika 1 <naomi.chika.1@gmail.com>
2021-06-24 14:48:49 +0300

    remove comma after url_for

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 
-from flask import render_template,redirect,url_for,
+from flask import render_template,redirect,url_for
 from . import auth
 from flask_login import login_user,logout_user,login_required
 
